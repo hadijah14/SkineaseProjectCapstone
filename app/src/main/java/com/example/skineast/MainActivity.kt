d060725f3bc423f81115d03177f03d06b1c8888f
Naufal Jakfar Ramadhan <95999282+naufaljakfar@users.noreply.github.com>
2023-06-05 22:57:16 +0700

    part 2

--- app/src/main/java/com/example/skineast/MainActivity.kt
+++ app/src/main/java/com/example/skineast/MainActivity.kt
@@ -1,64 +1,41 @@
 package com.example.skineast
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Size
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import android.view.TextureView
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
-class MainActivity : AppCompatActivity() {
-    private val CAMERA_PERMISSION_CODE = 1
-
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val captureButton: Button = findViewById(R.id.capture_button)
-        captureButton.setOnClickListener {
-            if (checkCameraPermission()) {
-                // Panggil metode untuk mengambil gambar
-                captureImage()
-            } else {
-                requestCameraPermission()
-            }
-        }
+        val btnClick: Button = findViewById(R.id.btn_click)
+        btnClick.setOnClickListener(this)
     }
 
-    private fun captureImage() {
-        // Tambahkan kode di sini untuk mengambil gambar
-        Toast.makeText(this, "Mengambil gambar...", Toast.LENGTH_SHORT).show()
-    }
+    override fun onClick(v: View?) {
 
-    private fun checkCameraPermission(): Boolean {
-        val result = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-        return result == PackageManager.PERMISSION_GRANTED
-    }
-
-    private fun requestCameraPermission() {
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(Manifest.permission.CAMERA),
-            CAMERA_PERMISSION_CODE
-        )
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == CAMERA_PERMISSION_CODE) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Izin kamera diberikan
-                captureImage()
-            } else {
-                // Izin kamera ditolak
-                Toast.makeText(this, "Izin kamera ditolak.", Toast.LENGTH_SHORT).show()
+        if (v!=null){
+            when(v.id){
+                R.id.btn_click -> {
+                    val pindahIntent = Intent(this, HalamanKamera::class.java)
+                    startActivity(pindahIntent)
+                }
             }
         }
     }
