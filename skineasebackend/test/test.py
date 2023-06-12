37502daa130ed2084d2c50fde5c6f02ac03377a2
lailynurazizah <97577621+lailynurazizah@users.noreply.github.com>
2023-06-12 11:17:36 +0700

    Delete test.py

--- skineasebackend/test/test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-IMG_PATH = 'gambarkutil_1.jpg'
-SERVER = 'https://skineasepredict-ise5ka2eaq-et.a.run.app'
-
-with open(IMG_PATH, 'rb') as file:
-    resp = requests.post(SERVER, files={'file': file})
-
-print(resp.json())
