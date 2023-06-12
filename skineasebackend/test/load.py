dfe843919a47b7ba3a65cf38f16bc9eba7f522a8
lailynurazizah <97577621+lailynurazizah@users.noreply.github.com>
2023-06-12 11:19:00 +0700

    Add files via upload

---
+++ skineasebackend/test/load.py
@@ -0,0 +1,35 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import io
+from PIL import Image
+import base64
+from google.cloud import storage
+
+MODEL_PATH = 'models/Skinease_6.h5'
+model = keras.models.load_model(MODEL_PATH)
+
+def predict_img(input_image):
+    return prediction(model, input_image)
+
+def prediction(model, image_pred):
+    # Initialize the Cloud Storage client
+    storage_client = storage.Client()
+    bucket = storage_client.bucket('skinease-bucket')
+
+    # Download the image from Cloud Storage
+    blob = bucket.blob(image_pred)
+    image_bytes = blob.download_as_bytes()
+
+    img = Image.open(io.BytesIO(image_bytes)).convert('L')
+    img = img.resize((224, 224))
+    img = np.array(img) / 255
+    img = np.expand_dims(img, 0)
+
+    pred = model.predict(img)
+    pred0 = pred[0]
+    label0 = np.argmax(pred0)
+    return label0
