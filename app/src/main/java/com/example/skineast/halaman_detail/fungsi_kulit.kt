ad88264946e79a8546c58095b64ff9143fd34274
Naufal Jakfar Ramadhan <95999282+naufaljakfar@users.noreply.github.com>
2023-06-13 23:29:20 +0700

    part berapa gatau

---
+++ app/src/main/java/com/example/skineast/halaman_detail/fungsi_kulit.kt
@@ -0,0 +1,12 @@
+package com.example.skineast.halaman_detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.skineast.R
+
+class fungsi_kulit : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fungsi_kulit)
+    }
+}
---
+++ app/src/main/java/com/example/skineast/halaman_detail/kesalahan_memakai_tabir.kt
@@ -0,0 +1,12 @@
+package com.example.skineast.halaman_detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.skineast.R
+
+class kesalahan_memakai_tabir : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kesalahan_memakai_tabir)
+    }
+}
